4ded5897b90ccd272a75e85989e30c10286dd980
Wesllei <Garcia@macbook-pro-de-wesllei.local>
2015-07-30 19:40:36 -0400

    Adicionado ViewControllerQuadroHorario e botão que a liga na tela de perfil

---
+++ AppEscola/AppEscola/ViewControllerQuadroHorario.swift
@@ -0,0 +1,13 @@
+//
+//  ViewControllerQuadroHorario.swift
+//  AppEscola
+//
+//  Created by Wesllei on 30/07/15.
+//  Copyright © 2015 Wesllei. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerQuadroHorario: UIViewController {
+
+}
