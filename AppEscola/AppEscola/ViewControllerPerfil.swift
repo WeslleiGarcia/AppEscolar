aab89b6be5166a4ab17c3e12333af8f0f72eb374
Wesllei <Garcia@macbook-pro-de-wesllei.local>
2015-07-31 18:25:13 -0400

    Adicionado Função na Camera da Tela do Perfil.

--- AppEscola/AppEscola/ViewControllerPerfil.swift
+++ AppEscola/AppEscola/ViewControllerPerfil.swift
@@ -8,6 +8,38 @@
 
 import UIKit
 
-class ViewControllerPerfil: UIViewController {
+class ViewControllerPerfil: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imagePerfil: UIImageView!
+    
+    
+    @IBAction func btCamera(sender: AnyObject) {
+        
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        //picker.sourceType = .Photolibrary
+        
+        presentViewController(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imagePerfil.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+
+   
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
